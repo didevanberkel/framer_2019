22dc6fd1b97d42d6b10a6fd332894eab8a14d690
Dide van Berkel <dide.vanberkel@gmail.com>
2019-01-20 18:57:26 +0100

    Initial Commit

---
+++ Framer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Framer
+//
+//  Created by Dide van Berkel on 20/01/2019.
+//  Copyright © 2019 Dide van Berkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FramerTests/FramerTests.swift
@@ -0,0 +1,34 @@
+//
+//  FramerTests.swift
+//  FramerTests
+//
+//  Created by Dide van Berkel on 20/01/2019.
+//  Copyright © 2019 Dide van Berkel. All rights reserved.
+//
+
+import XCTest
+@testable import Framer
+
+class FramerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
