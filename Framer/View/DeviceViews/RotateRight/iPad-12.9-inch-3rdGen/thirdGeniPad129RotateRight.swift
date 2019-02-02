c44d3dbd1b6c46bcb997e90505e94c50714e4c38
Dide van Berkel <dide.vanberkel@gmail.com>
2019-02-02 10:22:09 +0100

    Finished iPhone 6.5 inch images

---
+++ Framer/View/DeviceViews/RotateRight/iPad-12.9-inch-3rdGen/thirdGeniPad129RotateRight.swift
@@ -0,0 +1,21 @@
+//
+//  thirdGeniPad129RotateRight.swift
+//  Framer
+//
+//  Created by Dide van Berkel on 27/01/2019.
+//  Copyright © 2019 Dide van Berkel. All rights reserved.
+//
+
+import UIKit
+
+class thirdGeniPad129RotateRight: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
